d813bf0c834819affde205c1d2689c6a54086dc2
Yonatan-Ethiopia <83534269+Yonatan-Ethiopia@users.noreply.github.com>
2025-05-10 20:10:23 +0300

    Update authController.js
    Added form validations, registration of users

--- src/authController.js
+++ src/authController.js
@@ -1,9 +1,21 @@
 const express = require('express')
-const bycrpt = require('bycrpt-hash')
+const bcrypt = require('bcrypt')
 const users = require('./src/models/usersModel')
-const Sign_In = (req, res)=>{
-  let { Name, Username, Password, email } = req.body;
-  if( !Name || !Username || !Password || !email ){
-    return res.status().json({ success: false, message: 'All fields must be filled' })
+const Sign_In = async (req, res)=>{
+  try{
+    const { Name, Username, Password, email } = req.body;
+    if( !Name || !Username || !Password || !email ){
+      return res.status().json({ success: false, message: 'All fields must be filled' })
+    }
+    presentU = await users.find({ Username })
+    presentE = await users.find({ email })
+    if( presentU ){
+      return res.status().json({ success: false, message: 'Username already taken'})
+    }
+    if( presentE ){
+      return res.status().json({ success: false, message: 'Email already exists' })
+    }
+    const hashedPass = bcrypt.hash( Password, 10)
+    await users.create({ Name, Username, hashedPass, email })
+    res.status().json({ success: false, message: 'Registered successfully'})
   }
-  Password = bycrpt(
