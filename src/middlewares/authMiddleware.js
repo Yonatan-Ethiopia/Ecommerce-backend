3dcf8a6fb9a22e05c1abff58601ead22c10eb393
Yonatan-Ethiopia <83534269+Yonatan-Ethiopia@users.noreply.github.com>
2025-05-12 21:08:15 +0300

    Update authMiddleware.js
    Made authMiddleware for verifying JWT tokens

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,1 +1,19 @@
-
+const jwt = require('jsonwebtoken')
+const blacklist = require('./src/blacklists')
+const verify = async (req, res, next)=>{
+  const auth = req.header[ 'authorization' ]
+  if(!auth){
+    return res.status().json({ success: false, message: 'Token not found' })
+  }
+  const token = auth.split([''], 1)
+  if(!token){
+    return res.status().json({ success: false, message: 'Invalid token' })
+  }
+  const isBlackList = await blacklist.find( token )
+  if(isBlackList){
+    return res.status().json({ success: false, message: 'Token is in blacklist' })
+  }
+  if(!process.env.JWT_SECRET){
+    return res.status().json({ success: false, message: 'Secret token empty' })
+  }
+  const is Verify = jwt.verify( process.env.JWT_SECRET, token)
