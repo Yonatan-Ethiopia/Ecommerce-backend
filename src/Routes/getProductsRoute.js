8b5a06f035f2e80ecfd19509a811cb073cc28693
Yonatan-Ethiopia <83534269+Yonatan-Ethiopia@users.noreply.github.com>
2025-05-14 22:29:08 +0300

    Update getProductsRoute.js

--- src/Routes/getProductsRoute.js
+++ src/Routes/getProductsRoute.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const route = Router.express()
-const getProducts = require('./src/controllers/getProducts')
+const getProducts = require('./src/controllers/productsController')
+const filter = require('./src/controllers/filterItems')
 const auth = require('./src/middlewares/authMiddleware')
 route.get('api/products', auth, getProducts)
+route.get('api/products/filter', filter)
 module.exports = route;
