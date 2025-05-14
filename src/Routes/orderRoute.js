da7bf57e1fd6153096818d1d7197777953f86717
Yonatan-Ethiopia <83534269+Yonatan-Ethiopia@users.noreply.github.com>
2025-05-14 22:26:49 +0300

    Update orderRoute.js

--- src/Routes/orderRoute.js
+++ src/Routes/orderRoute.js
@@ -1,1 +1,8 @@
-
+const express = require('express')
+const route = express.Route()
+const orderItem = require('./src/controllers/orderItemController')
+const orderCart = require('./src/controllers/orderCartController')
+const auth = require('./src/middlewares/authMiddleware')
+route.post('api/order/item', auth, orderItem)
+route.post('api/order/cart', auth, orderCart)
+module.exports = route
