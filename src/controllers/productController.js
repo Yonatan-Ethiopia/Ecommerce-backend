fbd0e6a7bccb64c99f3170c0a59d7abbc2ca0735
Yonatan-Ethiopia <83534269+Yonatan-Ethiopia@users.noreply.github.com>
2025-05-14 17:44:34 +0300

    Update productController.js

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,10 +3,10 @@ const products = require('products');
 const getProducts = async (req, res) => {
     try{
         const productList = await products.find();
-        res.json({post: `${productList}`});
+        res.json({ success: true, post: `${productList}`});
         console.log('Successfully got products');
     }catch(err){
-        res.json({message: 'Server error'});
+        res.status(500).json({message: 'Server error'});
     }
 }
-module.exports = getPtoducts ;
+module.exports = getProducts ;
