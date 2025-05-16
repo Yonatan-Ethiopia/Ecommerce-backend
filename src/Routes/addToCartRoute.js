3a4570111250c521f7a1da8295dd4c51e24456d3
Yonatan-Ethiopia <83534269+Yonatan-Ethiopia@users.noreply.github.com>
2025-05-16 19:50:02 +0300

    Update addToCartRoute.js

--- src/Routes/addToCartRoute.js
+++ src/Routes/addToCartRoute.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const route = Router.express()
-const addToCart = require('./src/controllers/addToCartController')
+const addToCart = require('./src/controllers/addCartController')
 const auth = require('./src/middlewares/authMiddleware')
 route.post('api/cart/Add', auth, addToCart)
 module.exports = route;
