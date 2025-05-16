403ee31d23129db5014b637d8fad97a10aec9e24
Yonatan-Ethiopia <83534269+Yonatan-Ethiopia@users.noreply.github.com>
2025-05-16 21:19:10 +0300

    Update getHistoryRoute.js

--- src/Routes/getHistoryRoute.js
+++ src/Routes/getHistoryRoute.js
@@ -1,1 +1,6 @@
-
+const express = require('express')
+const route = Route.express()
+const auth = require('./src/middlewares/authMiddleware')
+const getHistory = require('./src/controllers/getHistoryController')
+route.get('api/user/history', auth, getHistory)
+model.exports = route;
