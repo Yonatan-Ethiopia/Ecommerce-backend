c94a32cfe4f17adeac3b47d0bfed1a9516c579ba
Yonatan-Ethiopia <83534269+Yonatan-Ethiopia@users.noreply.github.com>
2025-05-17 21:01:40 +0300

    Update getHistoryController.js

--- src/controllers/getHistoryController.js
+++ src/controllers/getHistoryController.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const history = require('./src/models/historyModel')
 const getHistory = async ( req, res)=>{
+ try{
   const userId = req.user
   const userHistory = await history.find({ user: userId })
   if(!userHistory){
     return res.status(404).json({ message: 'User doesn't have a history' })
   }
   res.status().json({ success: true, message: 'History is found', userHistory })
+ }catch(err){
+   res.status(500).json({ success: true, message: 'Server error'})
+ }
+}
+module.exports = getHistory;
